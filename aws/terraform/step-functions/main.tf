ad83253b39d3e9ebbf7bac1438c992f1b726a448
Carlos Pulido Rosas <carlos.pulido.rosas@gmail.com>
2025-12-27 01:59:54 -0600

    Add EventBridge permissions to Step Functions role
    Added a new IAM role policy to allow Step Functions to manage EventBridge rules and targets. Also removed the unused GitHub Actions CI/CD workflow file.

--- aws/terraform/step-functions/main.tf
+++ aws/terraform/step-functions/main.tf
@@ -94,3 +94,23 @@ resource "aws_iam_role_policy" "ecs_run_task" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "eventbridge_permissions" {
+  name = "${local.prefix}-eventbridge-permissions"
+  role = aws_iam_role.step_functions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "events:PutTargets",
+          "events:PutRule",
+          "events:DescribeRule"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
